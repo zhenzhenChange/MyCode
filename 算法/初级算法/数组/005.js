b4d8536288077ebcf9489d74ce4d780bf9abc19b
xiaozhenzhen <1506678296@qq.com>
2020-04-03 23:54:08 +0800

    两道算法题 VS 一下午克隆大作战

---
+++ 算法/初级算法/数组/005.js
@@ -0,0 +1,16 @@
+var nums = [4, 1, 2, 1, 2];
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  nums.sort((a, b) => a - b);
+  
+  return (nums.join("@") + "@").replace(/(-?\d+@)\1*/g, whole => {
+    return whole.split("@").length - 1 === 1 ? parseFloat(whole) : "";
+  });
+};
+
+const res = singleNumber(nums);
+console.log(res);
---
+++ 算法/初级算法/数组/006.js
@@ -0,0 +1,21 @@
+var nums1 = [3, 1, 2],
+  nums2 = [1, 1];
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  var arr = [];
+  nums1.forEach(v => {
+    var index = nums2.indexOf(v);
+    if (index !== -1) {
+      nums2.splice(index, 1);
+      arr.push(v);
+    }
+  });
+  return arr;
+};
+var res = intersect(nums1, nums2);
+console.log(res);
