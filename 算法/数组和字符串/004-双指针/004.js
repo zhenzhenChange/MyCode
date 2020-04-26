61bd0ad577fcdb54a0842244a355809ba89dcfb0
xiaozhenzhen <1506678296@qq.com>
2020-04-26 21:05:01 +0800

    1道算法

--- 算法/数组和字符串/004-双指针/004.js
+++ 算法/数组和字符串/004-双指针/004.js
@@ -1,15 +1,16 @@
 /**
+ * 移除元素
+ *
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  let count = 0;
   for (let i = nums.length - 1; i >= 0; i--) {
-    if (nums[i] != val) count++;
+    if (nums[i] == val) nums.splice(i, 1);
   }
 
-  return count;
+  return nums.length;
 };
 
 var nums = [3, 2, 2, 3];
