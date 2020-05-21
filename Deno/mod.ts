90d32edcce54a10d38c914ac6bf0387bda48b7ee
xiaozhenzhen <150667829@qq.com>
2020-05-21 21:48:38 +0800

    学一点Deno的API

--- Deno/mod.ts
+++ Deno/mod.ts
@@ -60,3 +60,65 @@
 
 // console.log(Deno.execPath()); // => 返回当前 deno 可执行文件的路径
 // Deno.exit(5); // => 退出 Deno 进程，可以指定退出码，若无则为 0
+
+/* => 格式化诊断信息数组，并以用户友好的格式将其作为单个字符串返回
+  const [diagnostics, record] = await Deno.compile('./mod.ts');
+  console.log(Deno.formatDiagnostics(diagnostics as any));
+*/
+
+/* => 监听一个或多个路径的文件系统事件
+  const it = Deno.watchFs('/');
+  for await (const event of it) {
+    console.log(event);
+  } 
+*/
+
+// console.log(Deno.hostname()); // => 获取 Deno 进程所在的计算机主机名
+
+// console.log(Deno.isatty(Deno.openSync('./mod.ts').rid)); // => 检查指定的资源 rid 是否为终端
+// await Deno.link('./mod.ts', './mod2.ts'); // => 创建 newPath 作为 oldPath 的硬链接
+// Deno.linkSync('./mod.ts', './mod2.ts'); // => 同步
+
+// console.log(Deno.listen({ port: 3333 })); // => 在本地监听网络连接
+// console.log(Deno.listenTls()); // => 在本地监听来自 TLS 的网络连接
+// console.log(Deno.loadavg()); // => 平均负载度
+// console.log(await Deno.lstat('./mod.ts')); // => 解析给定的 path，并返回 Deno.FileInfo
+// console.log(Deno.lstatSync('./mod.ts')); // => 同步
+// console.log(await Deno.makeTempDir({ dir: './', prefix: 'zhenzhen', suffix: '123' })); // => 创建临时文件夹
+// console.log(Deno.makeTempDirSync({ dir: './', prefix: 'zhenzhen' })); // => 同步
+// console.log(await Deno.makeTempFile({ dir: './', suffix: '.txt' })); // => 创建临时文件
+// console.log(Deno.makeTempFileSync({ dir: './', suffix: '.txt' })); // => 同步
+// console.log(Deno.metrics()); // => 这主要用于 Deno 的开发
+// await Deno.mkdir('newDir'); // => 创建新目录 （文件夹已存在则报错）
+// Deno.mkdirSync("newDDD") // => 同步
+// console.log(Deno.osRelease()); // => 返回操作系统的发行版本
+// console.log(await Deno.open('./mod.ts')); // => 打开一个文件并异步返回一个 Deno.File 实例
+// console.log(Deno.openSync("./mod.ts")); // => 同步
+
+// => 打开并初始化插件
+// console.log(Deno.openPlugin('C:\\Users\\15066\\Desktop\\zhenzhen-Rush\\server\\.deno_plugins\\deno_mongo_7117ccb53f8978e745db83130ae27b42.dll'));
+
+/* => 从资源 rid 读取内容，并写入到数组缓冲区
+  const { rid } = Deno.openSync('./mod.ts');
+  const buffer = new Uint8Array(100000);
+  const result = await Deno.read(rid, buffer);
+  // const result = Deno.readSync(rid, buffer); // => 同步
+  console.log(result);
+  console.log(new TextDecoder().decode(buffer));
+*/
+
+// => 读取并将文件的全部内容解析为字节数组。 TextDecoder 可以在需要的情况下可以将字节转换成字符串
+// console.log(new TextDecoder("UTF-8").decode(Deno.readFileSync('./mod.ts')));
+// console.log(new TextDecoder('UTF-8').decode(await Deno.readFile('./mod.ts'))); // => 异步
+
+// => 读取 path 文件目录，并返回 Deno.DirEntry 迭代器（包含文件名，是否是文件、文件夹）
+// for await (const dir of Deno.readDir('../')) { // => 异步
+// for (const dir of Deno.readDirSync('../')) {
+//   console.log(dir);
+// }
+// await Deno.remove('./sss.ts'); // => 删除指定的文件或目录
+// Deno.removeSync('./sss.ts'); // => 同步
+
+// await Deno.rename('./aaa.ts', '../Array/bbb.ts'); // => 重命名并移动
+// Deno.renameSync('./aaa.ts', '../Array/bbb.ts'); // => 同步
+// console.log(Deno.resources()); // => 返回打开的文件资源 rid 及其字符串表示形式的 Map
